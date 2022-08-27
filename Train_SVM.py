281c92e90b6970796f12de6278de7cb64fc8a09d
fender2758 <hyonjun.kang.00@gmail.com>
2022-08-27 17:02:17 +0900

    hj server

--- Train_SVM.py
+++ Train_SVM.py
@@ -1,121 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-make model_preproccessed.pkl file.
-"""
-
-import matplotlib.pyplot as plt
+# -*- coding: utf-8 -*-import matplotlib.pyplot as plt
 import numpy as np
-import os
-import csv
-import librosa, numpy, pickle
+import librosa
 from scipy.fftpack import fft
 from sklearn.model_selection import train_test_split
 
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
-
-parentPath = '/home/kimminju/knot-server/real-data/'
-#types = ('sound', 'acc', 'gyro')
-def get_AllData():
-    # for path in os.listdir(parentPath):
-    #     if 'sound' in path:
-    #         paths['sound'].append(path)
-    #     elif 'acc' in path:
-    #         paths['acc'].append(path)
-    #     elif 'gyro' in path:
-    #         paths['gyro'].append(path)
-
-    #allData = {}
-    #for dataType in types:
-    #    allData[dataType]={}
-    allData={'sound':{}}
-    command_hash = {}
-    for path in os.listdir(parentPath):
-        if not '.csv' in path:
-            continue
-        # print(path)
-        #label,num,dataType = path.split('_')
-        command,label,num=path.split('_')
-        num=num.split('.')[0]
-        dataType='sound'
-
-        # if label != 'clap':
-        #     continue
-        
-        #dataType = dataType.split('.')[0]
-        
-        if not label in allData[dataType]:
-            command_hash[label] = command
-            allData[dataType][label] = {}
-        
-        allData[dataType][label][num] = {}
-        
-        nowData = allData[dataType][label][num]
-        
-        nowData['timestamp'] = []
-        #if dataType == 'sound':
-        #    nowData['value'] = []
-        #else :
-        #    for v in ('x', 'y', 'z'):
-        #        nowData[v] = []
-        
-
-        with open(os.path.join(parentPath, path)) as file:
-            reader = csv.reader(file)
-            nowData['value']=list(map(float,next(reader)))
-            #
-            ## i = 0
-            #for raw in reader:
-            #    if '#' in raw[0]:
-            #        continue
-            #    if raw[0] == '':
-            #        break
-            #    if dataType == 'sound':
-            #        nowData['value'].append(int(raw[0]))
-            #    else :
-            #        k = 0
-            #        for v in ('x', 'y', 'z'):
-            #            # print(raw[k])
-            #            nowData[v].append(float(raw[k]))
-            #            k+=1
-            #    # i+=1
-        
-        allData[dataType][label][num]['value']=np.array(nowData['value'],dtype=np.float32)[:4096]
-        #if dataType == 'sound':
-        #    allData[dataType][label][num]['value'] = np.array(nowData['value'], dtype=np.int16)[:4096]
-        #else :
-        #    for v in ('x', 'y', 'z'):
-        #        allData[dataType][label][num][v] = np.array(nowData[v], dtype=np.float32)[:8]
-    return allData, command_hash
-
-
-def get_dataset(allData):
-    data = []
-    labels = []
-    tabel = {}
-    for label in allData['sound']:
-        for num in allData['sound'][label]:
-            temp = allData['sound'][label][num]['value']
-            if label not in tabel:
-                tabel[label]=len(tabel.keys())
-            labels.append(tabel[label])
-
-            #for dataType in types:
-            #    if dataType == 'sound':
-            #        temp = allData[dataType][label][num]['value']
-            #        if label not in tabel:
-            #            tabel[label] = len(tabel.keys())
-            #        labels.append(tabel[label])
-            #    else :
-            #        for v in ('x', 'y', 'z'):
-            #            temp = np.concatenate((temp, allData[dataType][label][num][v]))
-                        
-            if len(data)==0:
-                data = np.array([temp])
-            else:
-                data = np.concatenate((data, [temp]))
-    return data, labels, tabel
-
 
 def get_mfccs(data):
     sample_rate=48000
@@ -128,63 +18,30 @@ def get_mfccs(data):
 
     delta2_mfcc = librosa.feature.delta(mfcc, order=2)
     
-    return delta2_mfcc
+    return delta2_mfcc	#코드 확인 필요
 
 def get_features(d):
-    d = np.array(d)
-    
+    d=np.array(d)
     """
     dataset_sound = np.array(sound[0])
     dataset_acc_x, dataset_acc_y, dataset_acc_z = np.array(acc).T
     dataset_gyr_x, dataset_gyr_y, dataset_gyr_z = np.array(gyr).T
     """
     dataset_sound = d[:4096]
+    
+    dataset_IMU = np.array_split(d[4096:], 6)
+    
+    for d in dataset_IMU:
+        d/=np.max(abs(d))
+        
+    
     #dataset_acc_x = d[4096:4104]
     #dataset_acc_y = d[4104:4112]
     #dataset_acc_z = d[4112:4120]
     #dataset_gyr_x = d[4120:4128]
     #dataset_gyr_y = d[4128:4136]
     #dataset_gyr_z = d[4136:4144]
-    dataset_sound_mag = fft(dataset_sound)
-    dataset_sound_mag = abs(dataset_sound_mag[:2049])
-    if 0 in dataset_sound_mag:
-        dataset_sound_mag = np.where(dataset_sound_mag==0, 0.0000001, dataset_sound_mag)
-    dataset_sound_mag_log = np.log(dataset_sound_mag)
-    
-    dataset_mfccs = get_mfccs(dataset_sound).flatten()
 
-
-    
-    #dataset_acc_x = np.pad(dataset_acc_x, ((0, 248)), 'constant')
-    #dataset_acc_x_mag = abs(fft(dataset_acc_x))
-    #
-    #dataset_gyr_z = np.pad(dataset_gyr_z, ((0, 248)), 'constant')
-    #dataset_gyr_z_mag = abs(fft(dataset_gyr_z))
-    
-    """
-    print(dataset_sound_mag)
-    print(dataset_sound_mag_log.dtype)
-    print(dataset_mfccs.dtype)
-    print(dataset_acc_x_mag.dtype)
-    print(dataset_gyr_z_mag.dtype)
-    """
-    return np.concatenate ((dataset_sound_mag, dataset_sound_mag_log, dataset_mfccs))
-
-
-def get_features_test(sound):
-    dataset_sound = np.array(sound.T[0])
-    #print(sound.shape)
-    #dataset_acc_x, dataset_acc_y, dataset_acc_z = np.array(acc).T
-    #dataset_gyr_x, dataset_gyr_y, dataset_gyr_z = np.array(gyr).T
-    """
-    dataset_sound = d[:4096]
-    dataset_acc_x = d[4096:4104]
-    dataset_acc_y = d[4104:4112]
-    dataset_acc_z = d[4112:4120]
-    dataset_gyr_x = d[4120:4128]
-    dataset_gyr_y = d[4128:4136]
-    dataset_gyr_z = d[4136:4144]
-    """
     dataset_sound_mag = fft(dataset_sound)
     dataset_sound_mag = abs(dataset_sound_mag[:2049])
     if 0 in dataset_sound_mag:
@@ -207,19 +64,19 @@ def get_features_test(sound):
     print(dataset_acc_x_mag.dtype)
     print(dataset_gyr_z_mag.dtype)
     """
-    return np.concatenate ((dataset_sound_mag, dataset_sound_mag_log, dataset_mfccs))
-
-def train():
-    allData, command_hash = get_AllData()
-    dataset, labels, table = get_dataset(allData)
-    #print(dataset)
-    with open('table.pkl', 'wb') as f:
-        pickle.dump(table, f)
-    with open('command.pkl', 'wb') as f:
-        pickle.dump(command_hash, f)
-    X_train, X_test, y_train, y_test = train_test_split(dataset, labels, test_size=0.33, random_state=42)
+    return np.concatenate (([dataset_sound_mag, dataset_sound_mag_log, dataset_mfccs], dataset_IMU))
 
 
+def train(userId):
+    dataset=[]
+    commandset=[]
+
+    ##DB
+    #for commandId,data FROM DATA:
+    #	dataset.append(data)	#np.arr로 바꿔서
+    #	commandset.append(commandId)
+
+    X_train, X_test, y_train, y_test = train_test_split(dataset, commandset, test_size=0.33, random_state=42)
 
     dataset_pre = np.zeros((len(X_train), 5138))
     dataset_test_pre = np.zeros((len(X_test), 5138))
@@ -238,20 +95,5 @@ def train():
 
     clf = svm.SVC(kernel='linear')
     clf.fit(dataset_pre, y_train)
-    with open('model_preproccessed.pkl', 'wb') as f:
-        pickle.dump(clf, f)
-
-
-
-# from sklearn.metrics import accuracy_score
-# y_pred = clf.predict(X_test)
-
-# print(accuracy_score(y_pred, y_test))
-
-# with open('model_preproccessed.pkl','wb') as f:
-#     pickle.dump(clf,f)
-  
-# with open('model_preproccessed.pkl', 'rb') as f:
-#     clf = pickle.load(f)
-
-# clf.predict([get_features(전처리)])
+	##DB
+	#insert into USER (model) values clf
--- server.py
+++ server.py
@@ -1,25 +1,19 @@
 #-*-  utf-8 -*-
 
 from flask import Flask,request
-import ssl,csv,time,os,gzip
-from collections import defaultdict
-import io
-import pickle
+import ssl,gzip
 import librosa
 import numpy as np
 from scipy.fftpack import fft
-import datetime
 from Train_SVM import *
 
-def socket_write(input_str):
-    client.send( bytes(input_str+'\n', 'utf-8'))
-def sound_augmentation_lib(value, noise_factor=400):
+def sound_augmentation_lib(value, noise_factor=400):	#사운드만 aug하고 나머지는 그냥 붙임
+	#return: 30 data with noise, 1 original data 
+	#value: 4096 sound, 24 acc, 24 gyro
     temp_data = []
     for k in range(10):
-        #data_up = np.concatenate((value[48:], np.zeros(48)))
-        #data_down = np.concatenate((np.zeros(48), value[:4048]))
         data_up = np.concatenate((value[48:], np.zeros(48)))
-        data_down = np.concatenate((np.zeros(48), value[:4096]))
+        data_down = np.concatenate((np.zeros(48), value[:4048]))
         
         noise = np.random.randn(len(value))
         augmented_data = value + noise_factor * noise
@@ -36,147 +30,90 @@ def sound_augmentation_lib(value, noise_factor=400):
         except:
             temp_data = np.array([augmented_data, augmented_data_up, augmented_data_down])
     return np.concatenate((temp_data, [value]))
-    
 
 LABEL='Label'
 CMD='cmd'
+USER_ID='user-id'
 TYPE='Type'
 TYPE_COLLECTOR='collector'
 TYPE_CLASSIFIER='classifier'
 TYPE_END='collectingEnd'
 
-primary_key_of_real=defaultdict(int)
-
-
-
-
-#train()
-with open('table.pkl', 'rb') as f:
-    key_table = pickle.load(f)
-with open('command.pkl', 'rb') as f:
-    command_table = pickle.load(f)
-table = {key_table[k]:k for k in key_table.keys()}
-print(table)
-
-for filename in os.listdir('real-data'):
-    try:
-        cmd,label,pkey=filename.split('_')
-        pkey=pkey.split('.')[0]
-    except:
-        print(filename)
-    else:
-        primary_key_of_real[label]=max(primary_key_of_real[label],int(pkey))
-print(primary_key_of_real.items())
-
-clf=0
-with open("model_preproccessed.pkl", "rb") as f:
-    clf = pickle.load(f)
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['POST'])
-def postJsonHandler():
-    global table,command_table
-    try:
-        #starttime=datetime.datetime.now()
-        sound=gzip.decompress(request.get_data()).decode("utf-8")
-        print("sound len:",len(sound))
-        print("sound type:",type(sound))
-        print("last ten sound:",sound[:10])
 
-        typeOfData=request.headers.get(TYPE)
-        #print('degzip time: ',(datetime.datetime.now()-starttime).microseconds)
+def generateUserId():
+    ##DB
+    #insert user into USER and get pk userid
+    return userId
 
-        #collecting data ends, train start
-        if typeOfData==TYPE_END:
-            train()
-            with open('table.pkl', 'rb') as f:
-                key_table = pickle.load(f)
-            with open('command.pkl', 'rb') as f:
-                command_table = pickle.load(f)
-            table = {key_table[k]:k for k in key_table.keys()}
-            print(table)
+def getCommandId(userId,labelOfData):
+	##DB
+	#check if there's already command in COMMAND
+		#generate new row and return pk commandId unless exist
+		#else, get and return commandId
+	return commandId
 
-        #collect data
-        elif typeOfData==TYPE_COLLECTOR:
-            cmdOfData=request.headers.get(CMD)
-            labelOfData=request.headers.get(LABEL)
-            pkeyDict=primary_key_of_real
+def getClf(userId):
+	##DB
+	#select model from USER where user-id = userId
+	return clf
 
-            #sound_data = list(int.from_bytes(sound[i*2:i*2+2], "big",signed=True) for i in range(4096))
-            #sound_data = np.array(sound_data)
-            #print(sound_data.shape)
 
-            sound_data=np.array(list(map(float,sound.rstrip(',').split(','))))
-            print(sound_data.shape)
-            print('last data:',sound_data[10:])
 
-            aug_sound_data = sound_augmentation_lib(sound_data, noise_factor=400)
-            print('last data after augm:',sound_data[10:])
-            for data in aug_sound_data:
-                pkeyDict[labelOfData]+=1
-                with open(f'real-data/{cmdOfData}_{labelOfData}_{pkeyDict[labelOfData]}.csv','w', newline='') as f:
-                    w=csv.writer(f)
-                    #w.writerow(data[i] for i in range(4096))
-                    w.writerow(data[i] for i in range(4144))
-        #classify
-        else:
-            try:
-                #starttime=datetime.datetime.now()
-                #feats = get_features_test(np.array([[float(int.from_bytes(sound[i*2:i*2+2], "big",signed=True))] for i in range(4096)])) #관측값들 입력
-                sound_data=map(float,sound.rstrip(',').split(','))
-                feats = get_features_test(np.array([[s] for s in sound_data])) #관측값들 입력
 
-                #print('get feats time: ',(datetime.datetime.now()-starttime).microseconds)
-                #starttime=datetime.datetime.now()
-                claass = clf.predict([feats])        #classifier 결과값
-                #print('get class time: ',(datetime.datetime.now()-starttime).microseconds)
-                print("class: "+table[claass[0]])
-                #print("real : "+labelOfData)
-                        
-                #send class to hub
-                if client:
-                    socket_write(command_table[table[claass[0]]])
-                return table[claass[0]]
-            
-            except Exception as e:
-                print(e)
-                return 'error'
+@app.route('/',methods=['GET','POST'])
+def index():
+	if request.method=='GET':
+		#initialize app
+		try:
+			userId=generateUserId()
+		except Exception as e:
+			print(e)
+			return 'cannot make user id'
+		else:
+			return userId
+	#POST
+	try:
+		raw_data=gzip.decompress(request.get_data()).decode("utf-8")
+		typeOfData=request.headers.get(TYPE)
+		userId=request.headers.get(USER_ID)
 
-    except Exception as e:
-        print(e)
-        return 'error'
-    else:
-        return 'done'
+		#collect ends, train start
+		if typeOfData==TYPE_END:
+			#!!!can change trainLabel to command-id in COMMAND
+			train(userId)
 
-import socket
-import time
-from _thread import *
-import atexit
-client=''
+		#collect data
+		elif typeOfData==TYPE_COLLECTOR:
+			cmdOfData=request.headers.get(CMD)	#ex) 442, 112
+			labelOfData=request.headers.get(LABEL)	#ex) door, desk
+			
+			temp=list(map(float,raw_data.rstrip(',').split(',')))
+			sound_data=np.array(temp[:4096])
+			accgyro_data=np.array(temp[-48:])
 
-def threaded():
-    global client
-    while True:
-        client, addr = s.accept()
-        print("new connection established!!")
-        #client.settimeout(5)
-        if not client.recv(1024):
-            print("disconnect!!!")
-            client.close()
+			aug_datas=sound_augmentation_lib(sound_data,noise_factor=400)
 
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.bind(('0.0.0.0', 8585 ))
-#s.listen(1)
-#start_new_thread(threaded,tuple())
+			commandId=getCommandId(userId,labelOfData)
 
-def handleExit():
-    global s,client
-    print("EXIT")
-    if client:
-        client.close()
-    s.close()
-#atexit.register(handleExit)
+			for d in aug_datas:
+				data=np.concatenate((d,accgyro_data))
+				##DB
+				#insert data into DATA with commandId
+
+		#classify
+		else:
+			temp_data=list(map(float,raw_data.rstrip(',').split(',')))
+			feats=get_features(temp_data)
+			clf=getClf(userId)
+			label=clf.predict([feats])	#originally label number, want to change to label
+			print(f'label: {label}')
+			return label
+
+	except Exception as e:
+		print(e)
+		return 'error'
 
 app.run(host='0.0.0.0', port= 9999, ssl_context='adhoc')
-
